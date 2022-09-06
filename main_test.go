0faa9be4899f534f0c4a86ae69c4829a403026dc
Trevor Sawler <trevor.sawler@gmail.com>
2022-09-06 16:32:42 -0300

    comments

--- main_test.go
+++ main_test.go
@@ -16,18 +16,18 @@ func Test_dineWithVaryingDelays(t *testing.T) {
 	}
 
 	for _, e := range theTests {
-		// set orderFinished to an empty slice of strings
+		// Set orderFinished to an empty slice of strings.
 		orderFinished = []string{}
 
-		// set times to 0 to speed things along
+		// Set all sleep times to 0 to speed things along.
 		eat = e.delay
 		sleepTime = e.delay
 		think = e.delay
 
-		// run the dine function
+		// Run the dine function.
 		dine()
 
-		// perform our tests
+		// Perform our test.
 		if len(orderFinished) != 5 {
 			t.Errorf("%s: incorrect length of slice orderFinished; expected 5 but got %d", e.name, len(orderFinished))
 		}
@@ -35,7 +35,7 @@ func Test_dineWithVaryingDelays(t *testing.T) {
 }
 
 func Test_dine(t *testing.T) {
-	// set times to 0 to speed things along
+	// Set all sleep times to 0 to speed things along.
 	eat = 0 * time.Second
 	sleepTime = 0 * time.Second
 	think = 0 * time.Second
