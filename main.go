a51e877755f894691cebebd3d8e255076ad0fd36
Trevor Sawler <trevor.sawler@gmail.com>
2022-09-07 09:42:15 -0300

    comments

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
+// The Dining Philosophers problem is well known in computer science circles.
+// Five philosophers, numbered from 0 through 4, live in a house where the
+// table is laid for them; each philosopher has their own place at the table.
+// Their only difficulty – besides those of philosophy – is that the dish
+// served is a very difficult kind of spaghetti which has to be eaten with
+// two forks. There are two forks next to each plate, so that presents no
+// difficulty. As a consequence, however, this means that no two neighbours
+// may be eating simultaneously.
+//
+// This is a simple implementation of Dijkstra's solution to the "Dining
+// Philosophers" dilemma.
+
 import (
 	"fmt"
 	"strings"
