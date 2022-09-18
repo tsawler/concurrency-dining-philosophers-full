d8f646a10ab32f8d568b0b0159ff54ebc05d3098
Trevor Sawler <trevor.sawler@gmail.com>
2022-09-18 09:55:34 -0300

    formatting & comments

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ package main
 // Five philosophers, numbered from 0 through 4, live in a house where the
 // table is laid for them; each philosopher has their own place at the table.
 // Their only difficulty – besides those of philosophy – is that the dish
-// served is a very difficult kind of spaghetti which has to be eaten with
+// served is a very special kind of spaghetti which has to be eaten with
 // two forks. There are two forks next to each plate, so that presents no
 // difficulty. As a consequence, however, this means that no two neighbours
 // may be eating simultaneously, since there are five philosophers and five forks.
@@ -134,7 +134,7 @@ func diningProblem(philosopher Philosopher, wg *sync.WaitGroup, forks map[int]*s
 		fmt.Printf("\t%s has both forks, and is eating.\n", philosopher.name)
 		time.Sleep(eatTime)
 
-		// The philosopher starts to thinkTime, but does not drop the forks yet.
+		// The philosopher starts to think, but does not drop the forks yet.
 		fmt.Printf("\t%s is thinking.\n", philosopher.name)
 		time.Sleep(thinkTime)
 
