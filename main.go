fa93fa250307a91c513ad954b9abc2691c03c2d9
Trevor Sawler <trevor.sawler@gmail.com>
2022-09-18 09:59:16 -0300

    comments

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ package main
 // Five philosophers, numbered from 0 through 4, live in a house where the
 // table is laid for them; each philosopher has their own place at the table.
 // Their only difficulty – besides those of philosophy – is that the dish
-// served is a very special kind of spaghetti which has to be eaten with
+// served is an unusual kind of spaghetti which has to be eaten with
 // two forks. There are two forks next to each plate, so that presents no
 // difficulty. As a consequence, however, this means that no two neighbours
 // may be eating simultaneously, since there are five philosophers and five forks.
--- readme.md
+++ readme.md
@@ -4,7 +4,7 @@ The Dining Philosophers problem is well known in computer science
 circles. Five philosophers, numbered from 0 through 4, live in a 
 house where the table is laid for them; each philosopher has their 
 own place at the table. Their only difficulty – besides those of 
-philosophy – is that the dish served is a very difficult kind of 
+philosophy – is that the dish served is an unusual kind of 
 spaghetti which has to be eaten with two forks. There are two forks 
 next to each plate, so that presents no difficulty: 
 as a consequence, however, no two neighbours may be eating simultaneously,
